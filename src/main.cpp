f9b694be19a701338f19adcff3d08a2944e053cc
Era James <51322608+era1112@users.noreply.github.com>
2024-01-04 17:56:30 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
 #include <WiFi.h>
 
-void setup() {
+void setup() 
+{
   Serial.begin(115200);
 
   WiFi.mode(WIFI_STA);
@@ -10,13 +11,19 @@ void setup() {
   Serial.println("***** Boot/setup complete *****");
 }
 
-void loop() {
+void loop() 
+{
   Serial.println("*** Start Scan ***");
-  int nets = WiFi.scanNetworks(false, true);    // These params set show_hidden == true
+  
+  // Returns number of networks found. The params include hidden networks (show_hidden == true)
+  int nets = WiFi.scanNetworks(false, true);    
+  
+
+  // Prints a table with SSID name, RSSI, and encryption type for all networks found
   Serial.print("Wifi networks detected: ");
   Serial.println(nets);
-
-  for (int i = 0; i < nets; ++i) {              //Reason  for Post Increment is to Increment the value then Assign it to "i"
+  for (int i = 0; i < nets; ++i)                //++i is to check if number of nets is > 0, then set i to 1 to begin table print 
+  {              
     //Print net details
     Serial.print(i + 1);
     Serial.print(":");
